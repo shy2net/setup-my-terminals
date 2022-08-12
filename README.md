21efb14cd13321ea13dc9d5ffb464a6417832441
shynet <shynet@gmail.com>
2022-08-12 22:07:01 +0300

    (init)

---
+++ README.md
@@ -0,0 +1,39 @@
+# Introduction
+
+This is a simple utility to setup OhMyZsh terminal with fonts and plugins like the following:
+
+- Git
+- Zsh-AutoSuggestions
+- Docker & Docker-Compose
+- Percol
+- Web-Search
+- Dirhistory
+
+It will also install the font `MesloLGS NF Regular` and other nerd fonts which is required by the terminal.
+Make sure after using this script to edit your Terminal font to use it, otherwise things would look a bit funny.
+
+## Running this utility
+
+Simply do the following (make sure to follow any prompts):
+
+```bash
+sh -c "$(curl -fsSL https://raw.github.com/shy2net/setup-my-terminals/master/linux.sh)"
+```
+
+## Pre-requisites
+
+This utility requires the following tools installed:
+
+- Git
+- Zsh
+- Python 2.x with Pip installed (required for Percol, Python 3 should work as well)
+
+
+### Installation commands for debian based Linux
+
+```bash
+# Run these under root
+sudo apt install git zsh python2
+curl https://bootstrap.pypa.io/pip/2.7/get-pip.py --output get-pip.py
+python2 get-pip.py
+```
---
+++ linux.sh
@@ -0,0 +1,46 @@
+#!/bin/sh
+
+# Check if oh-my-zsh is installed
+if [ ! -d "~/.oh-my-zsh"]; then
+    echo "oh-my-zsh not installed, downloading and installing now..."
+
+    # Clone ohmyzsh
+    sh -c "$(curl -fsSL https://raw.github.com/ohmyzsh/ohmyzsh/master/tools/install.sh)"
+fi
+
+echo "Installing nerd fonts..."
+
+# Install powerline nerd fonts
+git clone https://github.com/powerline/fonts
+sh -c fonts/install.sh
+rm -rf fonts
+
+# Install Meslo Regular fonts
+# TODO: This might not actually working, need to check it out
+curl -o "/usr/share/fonts/MesloLGS NF Regular.ttf" https://github.com/romkatv/powerlevel10k-media/raw/master/MesloLGS%20NF%20Regular.ttf
+
+# Install zsh-auto-suggestions
+git clone https://github.com/zsh-users/zsh-autosuggestions ${ZSH_CUSTOM:-~/.oh-my-zsh/custom}/plugins/zsh-autosuggestions
+
+# Check if percol is installed
+percol_installed=$(cat ~/.zshrc | grep percol)
+
+# TODO: It seems like percol is still not working, need to check why
+if [ $percol_installed -eq ""]; then
+    echo "Percol not found, installing..."
+    pip install percol || echo "Failed to install percol, check that python and pip are installed!"
+fi
+
+echo "Updating the plugins list for oh-my-zsh..."
+sed -i -r 's/plugins\=.+/plugins=(git docker docker-compose jsontools copyfile web-search dirhistory zsh-autosuggestions percol)/' ~/.zshrc
+
+# Install powerlevel10k
+git clone --depth=1 https://github.com/romkatv/powerlevel10k.git ${ZSH_CUSTOM:-$HOME/.oh-my-zsh/custom}/themes/powerlevel10k
+
+echo "Replacing theme to powerlevel10k..."
+sed -i -r 's/ZSH_THEME\=.+/ZSH_THEME=powerlevel10k\/powerlevel10k/' ~/.zshrc
+
+echo "Reloading oh-my-zsh..."
+
+# Reload oh-my-zsh
+source ~/.zshrc
